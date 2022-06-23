849ed72e2740e244c3d3af0b80da97e14e0af285
HoangDuc <hoangducpro001@gmail.com>
2022-06-23 23:32:19 +0700

    TaskListController
    - Create CRUD functions for TaskList

--- TodoApp_WebAPI/TodoApp_WebAPI/Controllers/TaskListsController.cs
+++ TodoApp_WebAPI/TodoApp_WebAPI/Controllers/TaskListsController.cs
@@ -20,5 +20,64 @@ namespace TodoApp_WebAPI.Controllers
         {
             _taskListRepository = taskListRepository;
         }
+
+        [HttpGet("userId")]
+        public async Task<ActionResult<IEnumerable<TaskList>>> GetTaskListsNotInsideAnyGroupByUserId(int userId)
+        {
+            return await _taskListRepository.GetAllListNotInsideAnyGroup(userId);
+        }
+
+
+        [HttpGet("{userId}/{groupId}")]
+        public async Task<ActionResult<IEnumerable<TaskList>>> GetTaskListInsideAGroupByUserId(int userId, int groupId)
+        {
+            return await _taskListRepository.GetAllListInsideAGroup(userId, groupId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateList(TaskList taskList)
+        {
+            try
+            {
+                taskList.Id = 0;
+                await _taskListRepository.CreateList(taskList);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTaskList( TaskList taskList)
+        {
+            try
+            {
+                await _taskListRepository.UpdateList(taskList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTaskList(int id)
+        {
+            try
+            {
+                await _taskListRepository.DeleteList(id);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
+        }
+
     }
 }
