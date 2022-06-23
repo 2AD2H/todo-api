a440e1437c0cb6440d8e69b7faaebac669966f70
letsmailhanh <letsmailhanh@gmail.com>
2022-06-23 23:26:36 +0700

    Update TasksController

--- TodoApp_WebAPI/TodoApp_WebAPI/Controllers/TasksController.cs
+++ TodoApp_WebAPI/TodoApp_WebAPI/Controllers/TasksController.cs
@@ -20,5 +20,60 @@ namespace TodoApp_WebAPI.Controllers
         {
             _taskRepository = taskRepository;
         }
+
+        [HttpGet("userId")]
+        public async Task<ActionResult<List<Models.Task>>> GetAllTaskNotInsideAList(int userId)
+        {
+            return await _taskRepository.GetAllTaskNotInsideAList(userId);
+        }
+
+        [HttpGet("{userId}/{listId}")]
+        public async Task<ActionResult<List<Models.Task>>> GetAllTaskInsideAList(int userId, int listId)
+        {
+            return await _taskRepository.GetAllTaskInsideAList(userId, listId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTask(Models.Task task)
+        {
+            try
+            {
+                task.Id = 0;
+                await _taskRepository.CreateTask(task);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTask(Models.Task task)
+        {
+            try
+            {
+                await _taskRepository.UpdateTask(task);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            try
+            {
+                await _taskRepository.DeleteTask(id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return NoContent();
+        }
     }
 }
