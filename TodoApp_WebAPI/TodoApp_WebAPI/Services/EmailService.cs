2119d08a76f202a21f99dab0cd15b73e8051d15e
letsmailhanh <letsmailhanh@gmail.com>
2022-07-24 08:08:02 +0700

    edit email content

--- TodoApp_WebAPI/TodoApp_WebAPI/Services/EmailService.cs
+++ TodoApp_WebAPI/TodoApp_WebAPI/Services/EmailService.cs
@@ -54,8 +54,15 @@ namespace TodoApp_WebAPI.Services
                 duedateMessage.IsBodyHtml = true;
                 string taskName = task.Name;
                 string taskDuedate = ((DateTime)task.DueDate).ToString("MM/dd/yyyy hh:mm tt");
-                duedateMessage.Body = "<h1>Hey there</h1><br/>Your task: <b>"+taskName+"</b> was due at "+taskDuedate;
-                duedateMessage.To.Add(emailTo);
+                //duedateMessage.Body = "<h1>Hey there</h1><br/>Your task: <b>"+taskName+"</b> was due at "+taskDuedate;
+                duedateMessage.Body =
+                    "<head><link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'></head><div class='card'><div class='card-header'>To Do App</div>" +
+                    "<div class='card-body'>"+
+                        "<h5 class='card-title'>Your task was due!</h5>"+
+                        "<p class='card-text'>Hello, your task <b>"+taskName+"</b> was due at " +taskDuedate +".</p>"+
+                    "</div>"+
+                    "</div>";
+                  duedateMessage.To.Add(emailTo);
             }
             catch (Exception)
             {
